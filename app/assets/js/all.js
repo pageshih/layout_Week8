4fa254142b57329b762cf047f2beb59d95a0662c
PageShih <page.acforshop@gmail.com>
2021-09-05 01:04:55 +0800

    9/4 update swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -6,7 +6,7 @@ $(function() {
 
 const swiper = new Swiper('.course-swiper', {
   watchSlidesProgress: true,
-  slidesPerView: 2,
+  slidesPerView: 1.34,
   spaceBetween: 30,
 });
 
@@ -14,6 +14,28 @@ var feedbackSwiper = new Swiper(".feedback-swiper", {
   slidesPerView: 3,
   slidesPerColumn: 2,
 //slidesPerColumnFill: "row",
-slidesPerGroup: 6,
-  spaceBetween: 30,
+  slidesPerGroup: 6,
+  spaceBetween: 16,
+  breakpoints:{
+    768:{
+      slidesPerView: 1,
+     slidesPerColumn: 3,
+  //slidesPerColumnFill: "row",
+    slidesPerGroup: 3,
+     spaceBetween: 8,
+     navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      
+    },
+    },
+    992:{
+    slidesPerView: 2,
+     slidesPerColumn: 2,
+  //slidesPerColumnFill: "row",
+    slidesPerGroup: 2,
+     spaceBetween: 16,
+    },
+  },
+     
 });
